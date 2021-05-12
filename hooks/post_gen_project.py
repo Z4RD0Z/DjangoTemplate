94a23bf66d79a7e88ea4383cc0b9b75cd6c698b8
Andrea Lomazzo <andrea_l@outlook.it>
2021-05-12 18:06:07 +0200

    fix:dummy value for secretkey

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,20 +3,21 @@ import os
 
 def generate_secret_key():
     """"""
-    pass
+    return "assdsadsadsajsajkdsakdjsakjdkKDKASJDk"
     #TODO
 
 
 def set_secret_key():
     """"""
 
-    secret_key = generate_secret_key
+    secret_key = generate_secret_key()
 
     with open(
             "../{{cookiecutter.project_dirname}}/{{cookiecutter.project_name}}/settings.py",
             "r+") as f:
 
-        file_contents = f.read().replace("!!!DJANGO_SECRET_KEY HERE!!!", )
+        file_contents = f.read().replace("!!!DJANGO_SECRET_KEY HERE!!!",
+                                         secret_key)
         f.seek(0)
         f.write(file_contents)
         f.truncate()
