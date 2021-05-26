7ce30e0e7f244f20d301c5de6b5dd68382ad898f
Andrea Lomazzo <andrea_l@outlook.it>
2021-05-26 17:08:46 +0200

    feat:create function for set db settings, create virtual env and install requirements

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,14 +1,27 @@
 import os
+import secrets
+import subprocess
+from pathlib import Path
+
+db = "{{cookiecutter.db}}"
 
 
 def generate_secret_key():
-    """"""
-    return "assdsadsadsajsajkdsakdjsakjdkKDKASJDk"
-    #TODO
+    """
+    Generate Secret key
+    """
+
+    length = 50
+    chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+
+    key = ''.join(secrets.choice(chars) for i in range(length))
+    return key
 
 
 def set_secret_key():
-    """"""
+    """
+    Add secret key to project settings
+    """
 
     secret_key = generate_secret_key()
 
@@ -23,9 +36,60 @@ def set_secret_key():
         f.truncate()
 
 
+def set_db_settings():
+    """[summary]
+    """
+    db_engine = ""
+    if db == "msql":
+        db_engine = "django.db.backends.mysql"
+    elif db == "postgres":
+        db_engine = "django.db.backends.postgresql_psycopg2"
+
+    with open(
+            "../{{cookiecutter.project_dirname}}/{{cookiecutter.project_name}}/settings.py",
+            "r+") as f:
+
+        file_contents = f.read().replace("CHANGE ENGINE HERE", db_engine)
+        f.seek(0)
+        f.write(file_contents)
+        f.truncate()
+
+
+def create_env():
+    """[summary]
+    """
+    try:
+        subprocess.run(["pyenv", 'install', "{{cookiecutter.python_version}}"])
+        subprocess.run([
+            'pyenv', 'virtualenv', "{{cookiecutter.python_version}}",
+            "{{cookiecutter.project_name}}"
+        ])
+    except subprocess.CalledProcessError as e:
+        print(e.output)
+
+
+def install_requirements():
+    """[summary]
+    """
+    try:
+        venv_dir = f"{Path.home()}/.pyenv/versions/django"
+
+        pip = os.path.join(venv_dir, 'bin', 'pip')
+
+        subprocess.run(args=[
+            pip, 'install', '-r',
+            '../{{cookiecutter.project_dirname}}/requirements.txt'
+        ])
+    except subprocess.CalledProcessError as e:
+        print(e.output)
+
+
 def main():
     """"""
     set_secret_key()
+    set_db_settings()
+    create_env()
+    install_requirements()
 
 
 if __name__ == "__main__":
